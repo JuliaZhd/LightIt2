61fe6aaa3812003a0047f7b3667442c3b3c591f5
JuliaZhd <fastomama@gmail.com>
2019-09-30 21:27:11 +0300

    add AltPerson

--- gamer.py
+++ gamer.py
@@ -1,8 +1,12 @@
 import random
 class Person:
+	#mark1 = 1/3
+	#mark2 = 2/3
 	def __init__(self, name, health):
 		self.name = name
 		self.health = health
+		self.mark1 = 1/3
+		self.mark2 = 2/3
 
 	def __repr__(self):
 		return f'У игрока {self.name} сейчас {self.health} здоровья'
@@ -11,14 +15,12 @@ class Person:
 	принимает случайное значение от 0 до 1 и это определяет какой вид воздействия сработает.
 	Далее рандомно определяется значение внутри заданного интервала. Выводится инфа кто и сколько получил.'''
 	def _rand_damage(self):
-		otm1 = 0.33
-		otm2 = 0.66
 		x = random.random()
-		if x< otm1:
+		if x< self.mark1:
 			y = random.randrange(18, 26, 1)
 			print (f"Игрок {self.name} получил повреждение 1 типа и теряет {y} процентов жизни")
 			return y
-		elif x < otm2:
+		elif x < self.mark2:
 			y = random.randrange(10, 36, 1)
 			print (f"Игрок {self.name} получил повреждение 2 типа и теряет {y} процентов жизни")
 			return y
@@ -28,27 +30,41 @@ class Person:
 
 	def hint(self):
 		self.health -= self._rand_damage()
+
+class AltPerson(Person):
+
+	def special_abilities(self):
+		if self.health < 35:
+			print("Для игрока Компьютер вводятся в силу новые условия!")
+			print("Вероятность его излечения возрастает на 20%!!!")
+			self.mark2 = 1 - (1/3)*1.2
+			self.mark1 = (1-self.mark2)/2
+			return self._rand_damage()
+		return self._rand_damage()
+
+	def hint(self):
+		self.health -= self.special_abilities()
 		
 
 class Game:
 	
 	def __init__(self):
-		self.person1 = Person("Computer",100)
+		self.person1 = AltPerson("Computer", 100)
 		self.name2 = input("Введите имя человека: ")
-		self.person2 = Person(self.name2,100)
+		self.person2 = Person(self.name2, 100)
 		print('')
 		print("Игра начинается! Представляем участников:")
 		print(self.person1)
 		print(self.person2)
 		print('')
 
-	#def set(self):
+	def set(self):
 		while self.person1.health > 0 and self.person2.health > 0:
-			z = random.choice([self.person1, self.person2])
-			z.hint()
-			print(z)
+			player = random.choice([self.person1, self.person2])
+			player.hint()
+			print(player)
 			print('')
-		print(f"Игра закончена! Проиграл игрок {z.name} :(")
+		print(f"Игра закончена! Проиграл игрок {player.name} :(")
 
-
-Game()
+if __name__ == '__main__':
+	Game()
